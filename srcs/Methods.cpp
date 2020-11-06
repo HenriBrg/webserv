71394e55bd2e47afbe8e3a65801c0eac696d46a0
AlexJeannot <alexandre.jeannot21@gmail.com>
2020-11-06 13:31:11 +0100

    oublie HEAD

--- srcs/Methods.cpp
+++ srcs/Methods.cpp
@@ -91,27 +91,8 @@ void Response::postReq(Request * req) {
 
         str = action == CREATE ? "201 - SUCCESSFULL POST REQUEST - CREATED FILE : " + req->file : "200 - SUCCESSFULL POST REQUEST - UPDATED FILE : " + req->file;
 
-<<<<<<< HEAD
-        // std::cout << RED << "====================================\n" << END;
-        // std::cout << str << std::endl;
-        // std::cout << contentLength << std::endl;
-        // if (_resBody)
-        //     std::cout << _resBody;
-        // std::cout << RED << "====================================\n" << END;
-
         _resBody = responseUtils::setBodyNoFile(str, str.size(), contentLength);
 
-        // std::cout << GREEN << "====================================\n" << END;
-        // //std::cout << _resBody << std::endl;
-        // std::cout << contentLength << std::endl;
-        // if (_resBody)
-        //     std::cout << _resBody << std::endl;
-        // std::cout << GREEN << "====================================\n" << END;
-
-=======
-        _resBody = responseUtils::setBodyNoFile(str, str.size(), contentLength);
-
->>>>>>> alex
         lastModified = ft::getLastModifDate(req->file);
         LOGPRINT(INFO, this, ("Response::postReq() : POST - Successfull POST request"));
     } 
